8721e71c910e4ab06781cb1b150f0421bb8c0b2d
kimyoungjin98 <gyu250@naver.com>
2021-11-07 19:49:44 +0900

    연습

---
+++ java/Main_03.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main_03 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+
+		int num = scan.nextInt();
+
+		for(int i = 1; i < 10 ; i++){
+			System.out.println(num + "*" + i + "=" + (num * i));
+		}
+
+	}
+	
+}
