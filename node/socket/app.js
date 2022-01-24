b2a90d0622aac7f227ee37ce4a88392338e842c0
kimyoungjin98 <gyu250@naver.com>
2022-01-25 00:00:36 +0900

    소켓

---
+++ node/socket/app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+app.listen(3000);
+const { WebSocketServer } = require("ws");
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on("connection", () => {
+  console.log("커넥션");
+});
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+module.exports = app;
